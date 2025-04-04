703809fd76f542037b4e44f4f639efb5cb9aa144
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-04-04 18:10:44 +0100

    feat: increase record limit in fetchEnrollee API call and add allEnrolleesChunk for bulk enrollee data retrieval

--- src/lib/services/fetch-enrolee.ts
+++ src/lib/services/fetch-enrolee.ts
@@ -46,7 +46,7 @@ export async function fetchEnrollee({
     const minimumID = (page - 1) * pageSize;
     const baseUrl = `${import.meta.env.VITE_PROGNOSIS_API_URL}/EnrolleeProfile/GetEnrolleeProvidersListsAll`;
 
-    const url = `${baseUrl}?schemeid=0&MinimumID=${minimumID}&NoOfRecords=20&pageSize=${pageSize}&ProviderName=${providerName}&TypeID=${typeId}&StateID=${stateId}&LGAID=0&enrolleeid=${enrolleeId}&provider_id=0`;
+    const url = `${baseUrl}?schemeid=0&MinimumID=${minimumID}&NoOfRecords=5000&pageSize=${pageSize}&ProviderName=${providerName}&TypeID=${typeId}&StateID=${stateId}&LGAID=0&enrolleeid=${enrolleeId}&provider_id=0`;
 
     console.log("Final API URL:", url);
 
