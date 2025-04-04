83f36360d1c94ef9357baa69ecee13cb639a6b87
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-04-04 14:51:28 +0100

    feat: add DownloadIcon component for file download functionality

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -184,3 +184,19 @@ export const SearchIcon = (props: IconSvgProps) => (
     />
   </svg>
 );
+
+export const DownloadIcon = (props: IconSvgProps) => (
+  <svg
+    aria-hidden="true"
+    focusable="false"
+    height="1em"
+    role="presentation"
+    viewBox="0 0 24 24"
+    width="1em"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="currentColor"
+    {...props}
+  >
+    <path d="M13.0001 16.1716L18.3641 10.8076L19.7783 12.2218L12.0001 20L4.22192 12.2218L5.63614 10.8076L11.0001 16.1716V4H13.0001V16.1716Z"></path>
+  </svg>
+);
