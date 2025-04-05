a5d1f147ed49bafc6774abca3cdc8031fac81d33
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-04-05 19:07:31 +0100

    feat: add fetchBenefitsById function and Benefit types for fetching enrollee benefits

---
+++ src/lib/services/fetch-benefit.ts
@@ -0,0 +1,41 @@
+export type Benefit = {
+  RowId: number,
+  Benefit: string,
+  Limit: string,
+  Used: number,
+  AmtClaimed: number,
+  Authorised: number,
+  Balance: string,
+  Scheme: number,
+  Service: number,
+  VisitsLimit: number,
+  VisitsUsed: number,
+  VisitsBalance: number,
+  CoinsurancePercentage: number,
+  CopaymentAmount: number,
+  ServiceLimitUnits: number
+}
+
+export type BenefitsResponse = {
+  status: number;
+  result: Benefit[];
+};
+
+export const fetchBenefitsById = async (
+  enrolleeId: string
+): Promise<BenefitsResponse | null> => {
+  try {
+    const response = await fetch(
+      `${import.meta.env.VITE_PROGNOSIS_API_URL}/EnrolleeProfile/GetEnrolleeBenefits?enrolleeid=${enrolleeId}`
+    );
+    if (!response.ok) {
+      throw new Error(`Failed to fetch enrollee data: ${response.status}`);
+    }
+    const data = (await response.json()) as BenefitsResponse;
+
+    return data || null;
+  } catch (error) {
+    console.error("Error fetching enrollee data:", error);
+    return null;
+  }
+};
