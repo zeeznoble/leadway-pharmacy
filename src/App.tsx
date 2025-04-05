8c0302f86d9894a58f1a44a816c12a7f41d85e94
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-04-05 19:04:34 +0100

    feat: add routes for ProviderPage and BenefitsPage in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 
 import IndexPage from "@/pages/index";
+import ProviderPage from "@/pages/providers";
+import BenefitsPage from "@/pages/benefits";
 
 function App() {
   return (
     <Routes>
       <Route element={<IndexPage />} path="/" />
+      <Route element={<ProviderPage />} path="/providers" />
+      <Route element={<BenefitsPage />} path="/benefits" />
     </Routes>
   );
 }
