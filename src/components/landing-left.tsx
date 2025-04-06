f4a6607d79669193f87480a554e6e4db5dd0d15b
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-04-06 10:26:38 +0100

    feat: add LandingLeft component for enhanced user interface

---
+++ src/components/landing-left.tsx
@@ -0,0 +1,16 @@
+import leadwayLogo from "/leadway-logo.png";
+
+export default function LandingLeft() {
+  return (
+    <div className="flex-1 left-image sm:flex hidden">
+      <div className="flex w-full h-full flex-col justify-between p-10">
+        <img src={leadwayLogo} alt="leadway_logo" className="w-72" />
+        <div>
+          <h2 className="text-4xl font-figtree font-bold text-white">
+            The Smarter Choice is Here...
+          </h2>
+        </div>
+      </div>
+    </div>
+  );
+}
