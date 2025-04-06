19c073416b3c6b16d31d6b6fa49dd79978469939
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-04-06 10:29:15 +0100

    fix: validate enrollee ID format and improve error handling in Input component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,8 @@ import { useNavigate } from "react-router-dom";
 import { Button } from "@heroui/button";
 import { Input } from "@heroui/input";
 
+import LandingLeft from "@/components/landing-left";
+
 import { fetchInfoAndRoute } from "@/lib/helpers";
 
 export default function Index() {
@@ -64,61 +66,74 @@ export default function Index() {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="bg-white rounded-lg shadow-xl p-8 w-full max-w-lg">
-        <img
-          src="/leadway-logo.png"
-          alt="Leadway"
-          className="w-48 mx-auto mb-6"
-        />
-        <h2 className="text-2xl font-bold text-center mb-6 text-gray-800">
-          Health Insurance Portal
-        </h2>
+    <div className="grid grid-cols-1 sm:grid-cols-2 h-screen">
+      <LandingLeft />
+      <div className="flex items-center justify-center min-h-screen">
+        <div className="w-full max-w-md">
+          {/* <img
+            src="/leadway-logo.png"
+            alt="Leadway"
+            className="w-48 mx-auto mb-6"
+          /> */}
+          {/* <h2 className="text-2xl font-bold text-center mb-6 text-gray-800">
+            Self Insurance Portal
+          </h2> */}
+          <div className="mb-7">
+            <h1 className="text-3xl font-figtree font-bold text-[#1A1A1A]">
+              Where genuine care meets{" "}
+              <span className="text-[#f15A24]">unparalleled service.</span>
+            </h1>
+            <p className="mt-6 text-md font-medium">
+              Access your insurance details—log in now.
+            </p>
+          </div>
+          <div className="mb-4">
+            <Input
+              label="Enrollee ID"
+              placeholder="Enter your Enrollee ID (e.g. 2400135/0)"
+              value={enrolleeId}
+              radius="sm"
+              size="lg"
+              onChange={(e) => setEnrolleeId(e.target.value)}
+              isInvalid={
+                (enrolleeId.trim() !== "" && !isValidId) || !!fetchError
+              }
+              errorMessage={
+                fetchError ||
+                (enrolleeId.trim() !== "" && !isValidId
+                  ? "ID must be a number followed by /0 (e.g. 2400135/0)"
+                  : "")
+              }
+              isDisabled={isLoadingPro}
+            />
+          </div>
 
-        <div className="mb-4">
-          <Input
-            label="Enrollee ID"
-            placeholder="Enter your Enrollee ID (e.g. 2400135/0)"
-            value={enrolleeId}
-            radius="sm"
-            size="lg"
-            onChange={(e) => setEnrolleeId(e.target.value)}
-            isInvalid={(enrolleeId.trim() !== "" && !isValidId) || !!fetchError}
-            errorMessage={
-              fetchError ||
-              (enrolleeId.trim() !== "" && !isValidId
-                ? "ID must be a number followed by /0 (e.g. 2400135/0)"
-                : "")
-            }
-            isDisabled={isLoadingPro}
-          />
-        </div>
-
-        <div className="flex flex-col sm:flex-row gap-4 mt-6 justify-center">
-          <Button
-            color="primary"
-            className="font-semibold"
-            size="lg"
-            radius="sm"
-            fullWidth
-            isDisabled={!isValidId || isLoadingPro || isLoadingBen}
-            isLoading={isLoadingPro}
-            onPress={() => handleProviderFetch("/providers")}
-          >
-            {isLoadingPro ? "Validating..." : "Check Providers"}
-          </Button>
-          <Button
-            color="warning"
-            className="font-semibold text-white"
-            size="lg"
-            fullWidth
-            radius="sm"
-            isDisabled={!isValidId || isLoadingBen || isLoadingPro}
-            isLoading={isLoadingBen}
-            onPress={() => handleBenefitsFetch("/benefits")}
-          >
-            {isLoadingBen ? "Validating..." : "Check Benefits"}
-          </Button>
+          <div className="flex flex-col gap-4 mt-6 justify-center">
+            <Button
+              color="primary"
+              className="font-semibold"
+              size="lg"
+              radius="sm"
+              fullWidth
+              isDisabled={!isValidId || isLoadingPro || isLoadingBen}
+              isLoading={isLoadingPro}
+              onPress={() => handleProviderFetch("/providers")}
+            >
+              {isLoadingPro ? "Validating..." : "Check Providers"}
+            </Button>
+            <Button
+              color="warning"
+              className="font-semibold text-white"
+              size="lg"
+              fullWidth
+              radius="sm"
+              isDisabled={!isValidId || isLoadingBen || isLoadingPro}
+              isLoading={isLoadingBen}
+              onPress={() => handleBenefitsFetch("/benefits")}
+            >
+              {isLoadingBen ? "Validating..." : "Check Benefits"}
+            </Button>
+          </div>
         </div>
       </div>
     </div>
