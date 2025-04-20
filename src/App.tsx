646acb769707f47830731f925a67590653f4ba01
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-04-20 11:28:55 +0100

    chore: bring in protectedRoute component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 
+import ProtectedRoute from "@/components/protected";
+
 import IndexPage from "@/pages/index";
 import LoginPage from "@/pages/login";
 
@@ -7,8 +9,14 @@ function App() {
   return (
     <Routes>
       <Route element={<LoginPage />} path="/auth/login" />
-
-      <Route element={<IndexPage />} path="/" />
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <IndexPage />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
   );
 }
