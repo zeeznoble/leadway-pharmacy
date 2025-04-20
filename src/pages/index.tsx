1414d28c43b826731edee5d7d6cd729a0a2e89e3
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-04-20 11:21:30 +0100

    chore: handle layout in route instead

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,7 @@
-import DefaultLayout from "@/layout/default";
-
 export default function IndexPage() {
   return (
-    <DefaultLayout>
-      <section>
-        <p>Dshshhshsh</p>
-      </section>
-    </DefaultLayout>
+    <section>
+      <p>Dshshhshsh</p>
+    </section>
   );
 }
