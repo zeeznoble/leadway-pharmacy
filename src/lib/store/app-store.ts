2abd33f8423829fde400ab4342e1b1f36e63aa87
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-04-20 11:21:47 +0100

    chore: AuthState, authStore

--- src/lib/store/app-store.ts
+++ src/lib/store/app-store.ts
@@ -1,9 +1,20 @@
 import { chunk } from "stunk";
 import { EnrolleeData } from "../services/fetch-enrolee";
+import { User } from "@/types";
 
 export const appChunk = chunk({
   enrolleeId: '',
   stateId: '',
   disciplineId: '',
   enrolleeData: null as EnrolleeData | null
+})
+
+type AuthState = {
+  user: User | null;
+  isLoading: boolean;
+};
+
+export const authStore = chunk<AuthState>({
+  isLoading: false,
+  user: null
 })
