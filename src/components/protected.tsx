8b3221c26e5d2c29b17c56f9c141a724c065ada0
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-04-20 11:30:02 +0100

    feat: ProtectedRoute compoenent

---
+++ src/components/protected.tsx
@@ -0,0 +1,24 @@
+import { Navigate } from "react-router-dom";
+
+import Spinner from "@/components/shared/spinner";
+import DefaultLayout from "@/layout/default";
+
+import { useAuth } from "@/lib/hooks/use-auth";
+
+export default function ProtectedRoute({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const { isAuthenticated, isLoading } = useAuth();
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  if (!isAuthenticated) {
+    return <Navigate to="/auth/login" replace />;
+  }
+
+  return <DefaultLayout>{children}</DefaultLayout>;
+}
