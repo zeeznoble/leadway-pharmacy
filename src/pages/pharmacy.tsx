2a35124531c687f45668c8542b20e93a42d90e8b
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-04-21 12:31:28 +0100

    refactor: Pharmacy compoenent to use hook instead of manual component state

--- src/pages/pharmacy.tsx
+++ src/pages/pharmacy.tsx
@@ -1,64 +1,32 @@
-import { useEffect, useState } from "react";
-
-import { useChunk } from "stunk/react";
-
 import { Input } from "@heroui/input";
 
 import SelectStates from "@/components/select-state";
-import ProvidersDataTable from "@/components/providers-table";
+import PharmacyDataTable from "@/components/pharmacy-table";
 
-import { appChunk } from "@/lib/store/app-store";
+import { useEnrolleeIdInput } from "@/lib/hooks/use-enrollee-input";
 
 export default function PharmacyPage() {
-  const [, setState] = useChunk(appChunk);
-  const [enrolleeId, setEnrolleeId] = useState("");
-  const [isValidId, setIsValidId] = useState(false);
-  const [fetchError, setFetchError] = useState("");
-  const [isLoading, _] = useState(false);
-
-  useEffect(() => {
-    const validateEnrolleeId = () => {
-      if (!enrolleeId.trim()) {
-        setIsValidId(false);
-        return;
-      }
-      const pattern = /^\d+\/0$/;
-      const isValid = pattern.test(enrolleeId.trim());
-      setIsValidId(isValid);
-
-      if (isValid) {
-        setFetchError("");
-      }
-    };
-
-    validateEnrolleeId();
-  }, [enrolleeId]);
+  const { enrolleeId, handleChange, handleBlur, isInvalid, errorMessage } =
+    useEnrolleeIdInput();
 
   return (
-    <section className="px-3">
-      <div className="bg-white py-6 rounded-lg">
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-          <Input
-            label="Enrollee ID"
-            placeholder="Enter your Enrollee ID (e.g. 2400135/0)"
-            value={enrolleeId}
-            radius="sm"
-            size="lg"
-            onChange={(e) => setEnrolleeId(e.target.value)}
-            onBlur={() => setState((state) => ({ ...state, enrolleeId }))}
-            isInvalid={(enrolleeId.trim() !== "" && !isValidId) || !!fetchError}
-            errorMessage={
-              fetchError ||
-              (enrolleeId.trim() !== "" && !isValidId
-                ? "ID must be a number followed by /0 (e.g. 2400135/0)"
-                : "")
-            }
-            isDisabled={isLoading}
-          />
-          <SelectStates />
-        </div>
+    <section className="py-3">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-5">
+        <Input
+          label="Enrollee ID"
+          placeholder="Enter your Enrollee ID (e.g. 2400135/0)"
+          value={enrolleeId}
+          radius="sm"
+          size="lg"
+          onChange={handleChange}
+          onBlur={handleBlur}
+          isInvalid={isInvalid}
+          errorMessage={errorMessage}
+          isDisabled={false}
+        />
+        <SelectStates />
       </div>
-      <ProvidersDataTable />
+      <PharmacyDataTable />
     </section>
   );
 }
