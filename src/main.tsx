f6654bf5cc316d0a43cba7e544907c009b9cb4b9
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-04-26 15:17:08 +0100

    chore: main app structure

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
+import { Toaster } from "react-hot-toast";
+
+import { initializeDiagnosesData } from "./lib/services/fetch-diagnosis.ts";
+
 import App from "./App.tsx";
 import { Provider } from "./provider.tsx";
+
 import "@/styles/globals.css";
+
+initializeDiagnosesData();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider>
         <App />
+        <Toaster
+          position="bottom-center"
+          toastOptions={{
+            duration: 5000,
+          }}
+        />
       </Provider>
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
