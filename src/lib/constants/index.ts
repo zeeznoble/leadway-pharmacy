507515f764376096dc8bb8af42c768a281d0e2b7
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-04-26 15:14:25 +0100

    feat: constants

---
+++ src/lib/constants/index.ts
@@ -0,0 +1,55 @@
+export const DELIVERY_COLUMNS = [
+  {
+    key: "enrollee",
+    label: "ENROLLEE",
+  },
+  {
+    key: "startDate",
+    label: "START DATE",
+  },
+  {
+    key: "nextDelivery",
+    label: "NEXT DELIVERY",
+  },
+  {
+    key: "frequency",
+    label: "FREQUENCY",
+  },
+  {
+    key: "status",
+    label: "STATUS",
+  },
+  {
+    key: "actions",
+    label: "ACTIONS",
+  },
+];
+
+export const BENEFITS_COLUMNS = [
+  { key: "Benefit", label: "BENEFIT" },
+  { key: "Limit", label: "LIMIT" },
+  { key: "Used", label: "USED" },
+  { key: "Balance", label: "BALANCE" },
+];
+
+
+export const PROVIDERS_COLUMNS = [
+  { key: "serial", label: "S/N" },
+  { key: "provider", label: "PROVIDER" },
+  // { key: "email", label: "EMAIL" },
+  { key: "ProviderAddress", label: "ADDRESS" },
+];
+
+export const ENROLLEE_COLUMNS = [
+  { key: "serial", label: "S/N" },
+  { key: "Member_FirstName", label: "First Name" },
+  { key: "Member_Surname", label: "Surname" },
+  { key: "Member_othernames", label: "Other Names" },
+  { key: "Member_Phone_One", label: "Member Phone" },
+  { key: "Member_EmailAddress_One", label: "Member EmailAddress" },
+  { key: "Member_Age", label: "Age" },
+  { key: "Member_maritalstatusDescr", label: "Marital Status" },
+  { key: "Member_Gender", label: "Gender" },
+  { key: "Member_MemberStatus_Description", label: "Status" },
+  { key: "Plan_Category", label: "Plan Category" },
+];
