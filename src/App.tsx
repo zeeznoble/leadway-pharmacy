199128cc589c556cba7e6fd2fc69e070c5260ae1
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-04-26 15:12:24 +0100

    chore: App.tsx refactor

--- src/App.tsx
+++ src/App.tsx
@@ -6,35 +6,18 @@ import IndexPage from "@/pages/index";
 import LoginPage from "@/pages/login";
 import EnrolleesPage from "@/pages/enrollees";
 import PharmacyPage from "@/pages/pharmacy";
+import DeliveriesPage from "./pages/deliveries";
 
 function App() {
   return (
     <Routes>
-      <Route element={<LoginPage />} path="/auth/login" />
-      <Route
-        path="/"
-        element={
-          <ProtectedRoute>
-            <IndexPage />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="/enrollees"
-        element={
-          <ProtectedRoute>
-            <EnrolleesPage />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="/pharmacy"
-        element={
-          <ProtectedRoute>
-            <PharmacyPage />
-          </ProtectedRoute>
-        }
-      />
+      <Route path="/auth/login" element={<LoginPage />} />
+      <Route element={<ProtectedRoute />}>
+        <Route path="/" element={<IndexPage />} />
+        <Route path="/enrollees" element={<EnrolleesPage />} />
+        <Route path="/pharmacy" element={<PharmacyPage />} />
+        <Route path="/deliveries" element={<DeliveriesPage />} />
+      </Route>
     </Routes>
   );
 }
