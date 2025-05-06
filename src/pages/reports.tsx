45ac828eec54654eccb12cb01351a5c4dfbd316b
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-05-06 10:25:19 +0100

    feat: reports page

---
+++ src/pages/reports.tsx
@@ -0,0 +1,9 @@
+export default function ReportsPage() {
+  return (
+    <section className="py-3">
+      <div className="flex justify-between items-center mb-6">
+        <h1 className="text-2xl font-bold">Reports</h1>
+      </div>
+    </section>
+  );
+}
