fa09cb70d28a237a586576fc4ef7bed905e4aed4
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-06-14 10:48:33 +0100

    feat: implement rider service functions for fetching and creating/updating riders

---
+++ src/lib/services/rider-service.ts
@@ -0,0 +1,54 @@
+import { CreateRiderRequest, Rider, RiderResponse } from "@/types";
+import { asyncChunk } from "stunk";
+import { API_URL } from "../helpers";
+
+export const fetchAllRiders = asyncChunk(async () => {
+  const response = await fetch(`${API_URL}/Riders/GetAllRiders`);
+  if (!response.ok) {
+    throw new Error('Failed to fetch riders');
+  }
+  const data = await response.json();
+  console.log(data);
+  return data.result as Rider[];
+});
+
+export const fetchRiderById = asyncChunk(async (riderId: number) => {
+  const response = await fetch(`${API_URL}/Riders/GetRiderByID?riderid=${riderId}`);
+  if (!response.ok) {
+    throw new Error('Failed to fetch rider');
+  }
+  const data = await response.json();
+  console.log(`${API_URL}/Riders/GetRiderByID?riderid=${riderId}`)
+  console.log(data);
+
+  return data.result[0] as Rider;
+});
+
+export const createOrUpdateRider = async (riderData: CreateRiderRequest | Rider): Promise<RiderResponse> => {
+  try {
+    const response = await fetch(`${API_URL}/Riders/UpsertRider`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(riderData),
+    });
+
+    if (!response.ok) {
+      throw new Error('Failed to save rider');
+    }
+
+    const data = await response.json();
+    const isUpdate = 'rider_id' in riderData && riderData.rider_id !== null;
+    return {
+      success: true,
+      message: isUpdate ? 'Rider updated successfully' : 'Rider created successfully',
+      response: data
+    };
+  } catch (error) {
+    return {
+      success: false,
+      message: error instanceof Error ? error.message : 'An error occurred',
+    };
+  }
+};
