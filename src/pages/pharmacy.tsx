6de83e6fa6b7e0f9604573ce7759a4b8730a8184
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-08-22 08:24:49 +0100

    chore: update phramacy page with the new hook to fetch enrolee, since we are using only enrollee id here, so we bring it in

--- src/pages/pharmacy.tsx
+++ src/pages/pharmacy.tsx
@@ -2,26 +2,25 @@ import { Input } from "@heroui/input";
 
 import SelectStates from "@/components/select-state";
 import PharmacyDataTable from "@/components/pharmacy-table";
-
-import { useEnrolleeIdInput } from "@/lib/hooks/use-enrollee-input";
+import { useEnrolleeSearch } from "@/lib/hooks/use-enrollee-search";
 
 export default function PharmacyPage() {
-  const { enrolleeId, handleChange, handleBlur, isInvalid, errorMessage } =
-    useEnrolleeIdInput();
+  const { searchCriteria, handleChange, handleBlur, validation } =
+    useEnrolleeSearch();
 
   return (
-    <section className="py-3">
+    <section>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-5">
         <Input
           label="Enrollee ID"
-          placeholder="Enter your Enrollee ID (e.g. 2400135/0)"
-          value={enrolleeId}
+          placeholder="Enter Enrollee ID (e.g. 2400135/0)"
+          value={searchCriteria.enrolleeId}
+          name="enrolleeId"
           radius="sm"
-          size="lg"
           onChange={handleChange}
           onBlur={handleBlur}
-          isInvalid={isInvalid}
-          errorMessage={errorMessage}
+          isInvalid={validation.enrolleeId.isInvalid}
+          errorMessage={validation.enrolleeId.errorMessage}
           isDisabled={false}
         />
         <SelectStates />
