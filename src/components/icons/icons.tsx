e30d497e2cc53b445709ec22d2d8329eae48369a
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-08-31 21:05:57 +0100

    feat: ArrowLeft

--- src/components/icons/icons.tsx
+++ src/components/icons/icons.tsx
@@ -200,3 +200,18 @@ export const SelectorIcon = (props: IconSvgProps) => {
     </svg>
   );
 };
+
+export const ArrowLeft = (props: IconSvgProps) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      width={props.width || 16}
+      height={props.height}
+      {...props}
+    >
+      <path d="M7.82843 10.9999H20V12.9999H7.82843L13.1924 18.3638L11.7782 19.778L4 11.9999L11.7782 4.22168L13.1924 5.63589L7.82843 10.9999Z"></path>
+    </svg>
+  );
+};
