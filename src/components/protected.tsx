e99a91bafe757faf0eb729dc84bf2d6acd019b8b
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-09-04 14:37:35 +0100

    feat: Added inactivity modal

--- src/components/protected.tsx
+++ src/components/protected.tsx
@@ -4,9 +4,22 @@ import Spinner from "@/components/shared/spinner";
 import DefaultLayout from "@/layout/default";
 
 import { useAuth } from "@/lib/hooks/use-auth";
+import { useInactivityModal } from "@/lib/hooks/use-inactive-modal";
+import { InactivityModal } from "./shared/inactivity-modal";
+import { logout } from "@/lib/store/app-store";
 
 export default function ProtectedRoute() {
   const { isAuthenticated, isLoading } = useAuth();
+
+  const handleLogout = () => {
+    logout();
+  };
+
+  const { showWarning, countdown, handleStayActive } = useInactivityModal({
+    warningTimeout: 5 * 60 * 1000,
+    logoutTimeout: 30 * 1000,
+    onLogout: handleLogout,
+  });
 
   if (isLoading) {
     return <Spinner />;
@@ -19,6 +32,12 @@ export default function ProtectedRoute() {
   return (
     <DefaultLayout>
       <Outlet />
+      <InactivityModal
+        isOpen={showWarning}
+        countdown={countdown}
+        onStayActive={handleStayActive}
+        onLogout={handleLogout}
+      />
     </DefaultLayout>
   );
 }
