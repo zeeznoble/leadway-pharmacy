1c0a9cbf7dc18b10e094714d76be224037e5816b
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-10-06 10:53:54 +0100

    chore: select cities

--- src/components/select-cities.tsx
+++ src/components/select-cities.tsx
@@ -64,7 +64,7 @@ export default function SelectCities({
 
       prevStateId.current = stateId;
 
-      citiesChunk.setParams(stateId);
+      citiesChunk.setParams({ stateId });
       citiesChunk.reload(stateId);
     }
   }, [stateId]);
